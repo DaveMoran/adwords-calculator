0ac5b67b3f27e3a33d7abc8c9d1705ed3c982ad2
Dave Moran <dave.moran118@gmail.com>
2017-08-24 17:48:32 -0400

    Move seperate parts of app to components

--- src/App.js
+++ src/App.js
@@ -28,36 +28,58 @@ class App extends Component {
         <div className="App-header">
           <h2>{title}</h2>
         </div>
-        <form>
-          <label for="monthlyBudget">Monthly Budget
-            <input id="monlthyBudget" type="number" />
-          </label> <br />
-          <label for="remainingBudget">Remaining Budget
-            <input id="remainingBudget" type="number" />
-          </label>
-        </form>
-        <hr />
-        <form>
-          <label for="campaign-1">Campaign 1
-            <input id="campaign-1" type="text" />
-          </label>
-          <label for="budget-1">Budget:
-            <input id="budget-1" type="number" />
-          </label> <br />
-          <label for="campaign-2">Campaign 2
-            <input id="campaign-2" type="text" />
-          </label>
-          <label for="budget-2">Budget:
-            <input id="budget-2" type="number" />
-          </label> <br />
-        </form>
-        <hr />
-        <div class="suggestions">
-          <div class="campaign-1">Campaign 1: $2000</div>
-          <div class="campaign-2">Campaign 2: $1000</div>
-        </div>
+        <Budget />
+        <Campaigns />
+        <Suggestions />
       </div>
     );
+  }
+}
+
+class Budget extends Component {
+  render() {
+    return (
+      <form>
+        <label for="monthlyBudget">Monthly Budget
+          <input id="monlthyBudget" type="number" />
+        </label> <br />
+        <label for="remainingBudget">Remaining Budget
+          <input id="remainingBudget" type="number" />
+        </label>
+      </form>
+    )
+  }
+}
+
+class Campaigns extends Component {
+  render() {
+    return (
+      <form>
+        <label for="campaign-1">Campaign 1
+          <input id="campaign-1" type="text" />
+        </label>
+        <label for="budget-1">Budget:
+          <input id="budget-1" type="number" />
+        </label> <br />
+        <label for="campaign-2">Campaign 2
+          <input id="campaign-2" type="text" />
+        </label>
+        <label for="budget-2">Budget:
+          <input id="budget-2" type="number" />
+        </label> <br />
+      </form>
+    )
+  }
+}
+
+class Suggestions extends Component {
+  render() {
+    return (
+      <div class="suggestions">
+        <div class="campaign-1">Campaign 1: $2000</div>
+        <div class="campaign-2">Campaign 2: $1000</div>
+      </div>
+    )
   }
 }
 
